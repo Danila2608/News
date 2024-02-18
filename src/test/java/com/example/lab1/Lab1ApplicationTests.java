d9d375a4cb4d96106136ab21b906ac1c4f2a2275
Danila <160331956+Danila2608@users.noreply.github.com>
2024-02-18 18:09:00 +0300

    Commit

--- src/test/java/com/example/lab1/Lab1ApplicationTests.java
+++ src/test/java/com/example/lab1/Lab1ApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.lab1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Lab1ApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    void testGetNews() {
+        ResponseEntity<Object> response = restTemplate.getForEntity("/news?category=general", Object.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
+    @Test
+    void testGetNewsInvalidCategory() {
+        ResponseEntity<Object> response = restTemplate.getForEntity("/news?category=invalid", Object.class);
+        assertNotEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode(), "Expected status code not to be INTERNAL_SERVER_ERROR");
+    }
 }
