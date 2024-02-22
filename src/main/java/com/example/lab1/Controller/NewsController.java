64480bb26bb7fa2c953b727586206f0a6e6d98ad
Danila <160331956+Danila2608@users.noreply.github.com>
2024-02-22 09:24:09 +0300

    Commit

--- src/main/java/com/example/lab1/Controller/NewsController.java
+++ src/main/java/com/example/lab1/Controller/NewsController.java
@@ -1,7 +1,6 @@
 package com.example.lab1.Controller;
 
 import com.example.lab1.Service.NewsService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,10 +17,6 @@ class NewsController {
 
     @GetMapping("/news")
     public ResponseEntity<Object> getNews(@RequestParam(value = "category", defaultValue = "general") String category) {
-        try {
-            return ResponseEntity.ok(newsService.getNews(category));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid category. Please use one of the following categories: business, entertainment, general, health, science, sports, technology.");
-        }
+        return ResponseEntity.ok(newsService.getNews(category));
     }
 }
